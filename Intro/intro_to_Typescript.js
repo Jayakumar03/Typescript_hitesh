502124d4f1d39ffb8132bcb8b5a65d22a66f7434
maverickJAI <jaimech61@gmail.com>
2022-09-09 21:33:29 +0530

    learned about types

---
+++ Intro/intro_to_Typescript.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+var user = {
+    name: "jayakumar",
+    age: 23
+};
+console.log(user.name);
---
+++ Intro/intro_to_Typescript.ts
@@ -0,0 +1,19 @@
+console.log("hello world")
+
+let user = {
+    name:"jayakumar",
+    age:23
+}
+
+console.log(user.name);
+
+
+// how to assign the type 
+let myname: string = "hello Jk"
+
+// typescript is does not camelcase and it's better to use variable names in lowercase
+let mynum = 25
+
+
+// if an error occurs an you need it to stop use export{}
+export{}
