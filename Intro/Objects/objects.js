105999786f4679db5a095a04015949a6779d6640
maverickJAI <jaimech61@gmail.com>
2022-09-16 21:54:19 +0530

    Learned about objects

---
+++ Intro/Objects/objects.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var User = {
+    name: "jayakumar",
+    ispaid: "true"
+};
+function createUser(name, ispaid) {
+}
+createUser(User);
+// how tell an fucntion or method i need object as return
+function name() {
+    return true;
+    // error because we didn't return as specified object.
+}
---
+++ Intro/Objects/objects.ts
@@ -0,0 +1,21 @@
+const User = {
+    name:"jayakumar",
+    ispaid:"true"
+}
+
+function createUser(name:string,ispaid:boolean) {
+    
+}
+
+createUser(user)
+// how tell an fucntion or method i need object as return
+function name():{
+    // this {} specific that return value is object
+    // we pass value or method or certain properties as well.
+    name:"jayakumar", ispaid:true
+} {
+    return true;
+    // error because we didn't return as specified object.
+    
+}
+export{}
